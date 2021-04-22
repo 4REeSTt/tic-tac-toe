964c2fdc2d22351f8ce2d9a827498df9a3a8a71b
Linus <69402183+4REeSTt@users.noreply.github.com>
2021-04-23 01:22:06 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include "Game_loop.hpp"
 
 int main(){
-	//Declare main game structure
+//Declare main game structure
     Game_structure Game_status;
-    //Start of the app
+//Start of the app
     Game_loop(Game_status);
 
     return 0;
